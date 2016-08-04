bc801c843320dbcdd206f7967a2989eb71b8e49a
yrsegal <yrsegal@gmail.com>
2016-08-04 09:35:12 -0400

    capitalization

--- src/main/java/wiresegal/silimatics/common/item/EnumSandType.kt
+++ src/main/java/wiresegal/silimatics/common/item/EnumSandType.kt
@@ -10,4 +10,24 @@ enum class EnumSandType(val color: Int, val chance: Double?) {
     BLOOD(0xBF8382), PAIN(0x628E66), SUN(0xC1A57A), VIEW(0xC88AD1);
 
     constructor(color: Int) : this(color, null)
+
+    override fun toString(): String {
+        return this.name.toLowerCase().split("_").joinToString("", transform = { it.capitalizeFirst() }).lowercaseFirst()
+    }
+
+    companion object {
+        fun String.capitalizeFirst(): String {
+            if (this.length == 0) return this
+            return this.slice(0..0).capitalize() + this.slice(1..this.length - 1)
+        }
+
+        fun String.lowercaseFirst(): String {
+            if (this.length == 0) return this
+            return this.slice(0..0).toLowerCase() + this.slice(1..this.length - 1)
+        }
+        
+        val sandTypeNames: List<String> by lazy {
+            EnumSandType.values().map { it.toString() }
+        }
+    }
 }
