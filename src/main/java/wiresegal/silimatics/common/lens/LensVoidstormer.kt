7392d4c12daf340452a07b66ac6d8e6e2b29b777
yrsegal <yrsegal@gmail.com>
2016-08-06 17:59:56 -0400

    make the windstormer's lens easier to extend for addons or w/e

--- src/main/java/wiresegal/silimatics/common/lens/LensVoidstormer.kt
+++ src/main/java/wiresegal/silimatics/common/lens/LensVoidstormer.kt
@@ -1,23 +1,11 @@
 package wiresegal.silimatics.common.lens
 
-import net.minecraft.entity.EntityLivingBase
-import net.minecraft.entity.player.EntityPlayer
-import net.minecraft.item.ItemStack
-import net.minecraft.util.SoundCategory
-import net.minecraft.world.World
-import wiresegal.silimatics.api.lens.ILens
-import wiresegal.silimatics.common.core.ModSoundEvents
-import wiresegal.silimatics.common.lib.LibMisc
-import wiresegal.zenmodelloader.client.core.TooltipHelper
+import net.minecraft.util.math.Vec3d
 
 class LensVoidstormer : LensWindstormer() {
 
-    override val mainDiv: Int
-        get() = -16
-    override val ySecDiv: Int
-        get() = -20
-    override val pitch: Float
-        get() = 0.75F
-    override val name: String
-        get() = "voidstormer"
+    override val mainDiv: Vec3d by lazy { Vec3d(-16.0, -20.0, -16.0) }
+    override val secDiv: Vec3d by lazy { Vec3d(-16.0, -16.0, -16.0) }
+    override val pitch: Float by lazy { 0.75F }
+    override val name: String by lazy { "voidstormer" }
 }
--- src/main/java/wiresegal/silimatics/common/lens/LensWindstormer.kt
+++ src/main/java/wiresegal/silimatics/common/lens/LensWindstormer.kt
@@ -5,6 +5,7 @@ import net.minecraft.entity.EntityLivingBase
 import net.minecraft.entity.player.EntityPlayer
 import net.minecraft.item.ItemStack
 import net.minecraft.util.SoundCategory
+import net.minecraft.util.math.Vec3d
 import net.minecraft.world.World
 import wiresegal.silimatics.api.lens.ILens
 import wiresegal.silimatics.common.core.ItemNBTHelper
@@ -18,14 +19,10 @@ open class LensWindstormer : ILens {
         val TAG_TICK = "tick"
     }
 
-    open val mainDiv: Int
-        get() = 16
-    open val ySecDiv: Int
-        get() = 20
-    open val pitch: Float
-        get() = 1.0F
-    open val name: String
-        get() = "windstormer"
+    open val mainDiv: Vec3d by lazy { Vec3d(16.0, 20.0, 16.0) }
+    open val secDiv: Vec3d by lazy { Vec3d(16.0, 16.0, 16.0) }
+    open val pitch: Float by lazy { 1.0F }
+    open val name: String by lazy { "windstormer" }
 
     override fun onUsingTick(world: World, player: EntityPlayer, stack: ItemStack) {
         if (player.isSneaking) {
@@ -34,9 +31,9 @@ open class LensWindstormer : ILens {
             val lookVec = player.lookVec
             if (tick % (20 / pitch).toInt() == 0)
                 world.playSound(null, player.posX, player.posY, player.posZ, ModSoundEvents.WOOSH, SoundCategory.PLAYERS, 1f, pitch)
-            player.motionX -= lookVec.xCoord / mainDiv
-            player.motionY -= lookVec.yCoord / ySecDiv
-            player.motionZ -= lookVec.zCoord / mainDiv
+            player.motionX -= lookVec.xCoord / mainDiv.xCoord
+            player.motionY -= lookVec.yCoord / mainDiv.yCoord
+            player.motionZ -= lookVec.zCoord / mainDiv.zCoord
             val posVec = player.positionVector
             val range = 16.0
             val entities = world.getEntitiesWithinAABBExcludingEntity(player, player.entityBoundingBox.expand(range, range, range))
@@ -44,9 +41,9 @@ open class LensWindstormer : ILens {
                 val dirVec = entity.positionVector.subtract(posVec).normalize()
                 val dot = dirVec.dotProduct(lookVec)
                 if (dot > LOOK_THRESHOLD) {
-                    entity.motionX += lookVec.xCoord / mainDiv
-                    entity.motionY += lookVec.yCoord / mainDiv
-                    entity.motionZ += lookVec.zCoord / mainDiv
+                    entity.motionX += lookVec.xCoord / secDiv.xCoord
+                    entity.motionY += lookVec.yCoord / secDiv.yCoord
+                    entity.motionZ += lookVec.zCoord / secDiv.zCoord
                 }
             }
         } else
