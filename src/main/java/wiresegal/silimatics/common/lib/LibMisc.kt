86c62ebc081d908038a70194fc90605b6545be67
yrsegal <yrsegal@gmail.com>
2016-08-07 12:34:24 -0400

    i am a fool, this is a temporary fix

--- src/main/java/wiresegal/silimatics/common/lib/LibMisc.kt
+++ src/main/java/wiresegal/silimatics/common/lib/LibMisc.kt
@@ -4,7 +4,7 @@ object LibMisc {
     const val MODID = "smedry"
     const val VERSION = "1.0"
     const val NAME = "Silimatics"
-    const val DEPS = "required-before:zml@[1.4,)"
+    const val DEPS = "required-before:zml"
 
     const val COMMON_PROXY = "wiresegal.silimatics.common.core.CommonProxy"
     const val CLIENT_PROXY = "wiresegal.silimatics.client.core.ClientProxy"
