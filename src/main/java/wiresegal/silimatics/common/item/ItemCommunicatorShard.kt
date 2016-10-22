621668e8d25dc56cd19339e8d8cd37c2a2a3d396
Elad Kinsbruner <eladkay@gmail.com>
2016-10-22 16:58:30 +0300

    Coooolors!

--- src/main/java/wiresegal/silimatics/common/item/ItemCommunicatorShard.kt
+++ src/main/java/wiresegal/silimatics/common/item/ItemCommunicatorShard.kt
@@ -5,6 +5,7 @@ import net.minecraft.entity.Entity
 import net.minecraft.entity.player.EntityPlayer
 import net.minecraft.item.ItemStack
 import net.minecraft.util.text.TextComponentString
+import net.minecraft.util.text.TextFormatting
 import net.minecraft.world.World
 import net.minecraftforge.common.MinecraftForge
 import net.minecraftforge.event.ServerChatEvent
@@ -12,6 +13,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import wiresegal.silimatics.common.core.ItemNBTHelper
 import wiresegal.silimatics.common.core.ModItems
 import wiresegal.silimatics.common.lib.LibNames
+import wiresegal.silimatics.common.util.color
 import wiresegal.zenmodelloader.common.core.IItemColorProvider
 import wiresegal.zenmodelloader.common.items.base.ItemMod
 import java.util.*
@@ -21,7 +23,22 @@ import java.util.*
  */
 class ItemCommunicatorShard : ItemMod(LibNames.COMMUNICATOR_SHARD), IItemColorProvider {
     override fun getItemColor(): IItemColor? {
-        return IItemColor {stack, i -> EnumSandType.VIEW.glassColor}
+        return IItemColor { stack, i -> getColor(stack) }
+    }
+
+    fun getColor(stack: ItemStack) = EnumSandType.VIEW.glassColor + ItemNBTHelper.getUUID(stack, "uuid", false)!!.hashCode() / 2
+
+    fun getClosestColor(color: Int): TextFormatting {
+        var distance = Math.abs(TextFormatting.values()[0].color()!! - color)
+        var idx = 0
+        for(c in TextFormatting.values().indices.filter { it != 0 && TextFormatting.values()[it].color() != null}) {
+            val cDistance = Math.abs(TextFormatting.values()[c].color()!! - color)
+            if(cDistance < distance) {
+                idx = c
+                distance = cDistance
+            }
+        }
+        return TextFormatting.values()[idx]
     }
 
     init {
@@ -34,7 +51,7 @@ class ItemCommunicatorShard : ItemMod(LibNames.COMMUNICATOR_SHARD), IItemColorPr
             if(player.heldItemMainhand != null && player.heldItemMainhand?.item == this &&
                     ItemNBTHelper.getUUID(player.heldItemMainhand!!, "uuid", true) ==
                             ItemNBTHelper.getUUID(ev.player.heldItemMainhand!!, "uuid", true))
-                                player.addChatComponentMessage(TextComponentString("Communicator's Shard: ").appendSibling(ev.component))
+                                player.addChatComponentMessage(TextComponentString(getClosestColor(getColor(player.heldItemMainhand!!)).toString() + "Communicator's Shard: ").appendSibling(ev.component))
         ev.isCanceled = true
     }
     override fun addInformation(stack: ItemStack, playerIn: EntityPlayer?, tooltip: MutableList<String>, advanced: Boolean) {
---
+++ src/main/java/wiresegal/silimatics/common/util/LibExtensions.kt
@@ -0,0 +1,33 @@
+package wiresegal.silimatics.common.util
+
+import net.minecraft.util.text.TextFormatting
+
+/**
+ * Created by Elad on 10/22/2016.
+ */
+fun TextFormatting.color(): Int? = when(this) {
+    TextFormatting.BLACK -> 0x000000
+    TextFormatting.DARK_BLUE -> 0x0000AA
+    TextFormatting.DARK_GREEN -> 0x00AA00
+    TextFormatting.DARK_AQUA -> 0x00AAAA
+    TextFormatting.DARK_RED -> 0xAA0000
+    TextFormatting.DARK_PURPLE -> 0xAA00AA
+    TextFormatting.GOLD -> 0xFFAA00
+    TextFormatting.GRAY -> 0xAAAAAA
+    TextFormatting.DARK_GRAY -> 0x555555
+    TextFormatting.BLUE -> 0x5555FF
+    TextFormatting.GREEN -> 0x55FF55
+    TextFormatting.AQUA -> 0x55FFFF
+    TextFormatting.RED -> 0xFF5555
+    TextFormatting.LIGHT_PURPLE -> 0xFF55FF
+    TextFormatting.YELLOW -> 0xFFFF55
+    TextFormatting.WHITE -> 0xFFFFFF
+    TextFormatting.OBFUSCATED -> null
+    TextFormatting.BOLD -> null
+    TextFormatting.STRIKETHROUGH -> null
+    TextFormatting.UNDERLINE -> null
+    TextFormatting.ITALIC -> null
+    TextFormatting.RESET -> null
+}
+
+
